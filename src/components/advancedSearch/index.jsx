67bb8a45f6c238181771d599b3adf7979fc4a57b
Asad Ali Asim <asadaliasim909@gmail.com>
2024-11-09 20:19:09 +0500

    fix: error

--- src/components/advancedSearch/index.jsx
+++ src/components/advancedSearch/index.jsx
@@ -1,5 +1,5 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react';
-
 const SearchFilter = ({ onFilterChange }) => {
   const [filters, setFilters] = useState({
     location: '',
